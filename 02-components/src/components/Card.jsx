d85e48419543cfac0448e134c4f3cec1d8623107
pushpendra02thakur <pushpendrathakur0212@gmail.com>
2026-04-21 23:52:15 +0530

    02-components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <h1>Pushpendra Chauhan</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, dolorum.</p>
+          </div>
+    </div>
+  )
+}
+
+export default Card
